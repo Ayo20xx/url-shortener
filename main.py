fc3cab9d647cb5386a7b876ae3560060129f6d10
Ayo20xx <ayomidevictoraaaa@gmail.com>
2026-05-04 17:52:41 +0100

    add working url shortener with in-memory storage

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from urllib.parse import urlparse, urlunparse
-user=input("enter url")
-rs=urlparse(user)
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
+from secrets import token_urlsafe
+from typing import Any
 
-while True:
-    if rs.scheme == "http"or "https" :
-        print("this is a legit url")
-         break
-    else:  
+app = FastAPI()
+database = {}
+
+def generate_code():
+    return token_urlsafe(6)
+
+@app.post("/url-shorten")
+def short_link(url: str) -> dict[str, Any]:
+    for code, link in database.items():
+        if link == url:
+            return {"shortened": code}
+    code = generate_code()
+    database[code] = url
+    return {"shortened": code}
+
+@app.get("/{short_code}")
+def redirect_to_url(short_code: str):
+    if short_code not in database:
+        raise HTTPException(status_code=404, detail="URL not found")
+    return RedirectResponse(status_code=302, url=database[short_code])
