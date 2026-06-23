a308cdfaa14b882540824f5268af291e17793caa
Ayo20xx <ayomidevictoraaaa@gmail.com>
2026-06-23 14:58:18 +0100

    fix

--- app/config.py
+++ app/config.py
@@ -1,8 +1,10 @@
+from typing import Optional
+
 from pydantic_settings import BaseSettings,SettingsConfigDict
 
 class urlsettings(BaseSettings):
     DATABASE_URL :str
-    BASE_URL : str
+    BASE_URL: Optional[str] = None
     
     model_config= SettingsConfigDict(
         env_file=".env",
